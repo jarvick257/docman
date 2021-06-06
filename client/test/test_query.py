5a0792e4d662e40cd5df1e8f552ae55e449eb942
Jonas Wallmeier <jonas@wallmeier.name>
2021-06-06 19:30:47 +0200

    query test

---
+++ client/test/test_query.py
@@ -0,0 +1,81 @@
+from collections import namedtuple
+import requests
+import pytest
+
+import docman
+from docman.cli.query import _run
+from testhelpers import RequestsMock, with_test_config
+
+
+def args(**kwargs):
+    fake_args = namedtuple(
+        "args", ("id", "text", "tags", "date_from", "date_until", "short", "Q", "raw")
+    )
+    return fake_args(
+        kwargs.get("id", None),
+        kwargs.get("text", None),
+        kwargs.get("tags", None),
+        kwargs.get("date_from", None),
+        kwargs.get("date_until", None),
+        kwargs.get("short", False),
+        kwargs.get("Q", True),
+        kwargs.get("raw", False),
+    )
+
+
+def test_query_noarg(with_test_config):
+    doc = docman.Document.load({})
+    mock = RequestsMock()
+    requests.get = mock.get
+    mock.set_response("{}", 200)
+    assert _run(doc, args()) == (None, 0)
+    assert mock.url == "http://localhost:8123/query"
+    assert mock.data == {}
+    assert mock.files is None
+
+
+def test_query_args(with_test_config):
+    doc = docman.Document.load({})
+    mock = RequestsMock()
+    requests.get = mock.get
+    mock.set_response("{}", 200)
+    args_d = dict(
+        id="id",
+        text="text",
+        tags="some,tags",
+        date_from="2020-01-01",
+        date_until="2023-01-01",
+    )
+    assert (
+        _run(
+            doc,
+            args(**args_d),
+        )
+        == (None, 0)
+    )
+    # request data is same as arguments except tags are split
+    req_d = args_d
+    req_d["tags"] = ["some", "tags"]
+    assert mock.url == "http://localhost:8123/query"
+    assert mock.data == req_d
+    assert mock.files is None
+
+
+def test_query_wrong_date(capfd, with_test_config):
+    doc = docman.Document.load({})
+    mock = RequestsMock()
+    requests.get = mock.get
+    mock.set_response("{}", 200)
+    assert _run(doc, args(date_from="20201201")) == (None, 1)
+    out, err = capfd.readouterr()
+    assert err == ""
+    assert out == "Date must be in YYYY-MM-DD format.\n"
+
+    doc = docman.Document.load({})
+    mock = RequestsMock()
+    requests.get = mock.get
+    mock.set_response("{}", 200)
+    assert _run(doc, args(date_until="2020-13-01")) == (None, 1)
+    out, err = capfd.readouterr()
+    assert err == ""
+    assert out == "Date must be in YYYY-MM-DD format.\n"
